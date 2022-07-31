74edb1fd6a8448986c430ec554c832623ffd7fce
daisyalexandria <dazavala7@gmail.com>
2022-07-31 11:07:34 -0500

    created front end with multiple view pages (about, test, signup, login, logout). user is able to sign up, login, and log out

---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
+import TestView from "../views/TestView.vue";
+import SignupView from "../views/SignupView.vue";
+import LoginView from "../views/LoginView.vue";
+import LogoutView from "../views/LogoutView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: AboutView,
+  },
+  {
+    path: "/test",
+    name: "test",
+    component: TestView,
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: SignupView,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginView,
+  },
+  {
+    path: "/logout",
+    name: "logout",
+    component: LogoutView,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
---
+++ src/views/LogoutView.vue
@@ -0,0 +1,16 @@
+<script>
+import axios from "axios";
+
+export default {
+  created: function () {
+    delete axios.defaults.headers.common["Authorization"];
+    localStorage.removeItem("jwt");
+    this.$router.push("/");
+    console.log("successfully logged out");
+  },
+};
+</script>
+
+<template>
+  <div class="logout"></div>
+</template>
